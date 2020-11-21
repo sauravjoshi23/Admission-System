2454e0451e82b553459437f753c358fc2ae56eff
akshit6 <akshit.gs@somaiya.edu>
2020-11-21 16:55:23 +0530

    added import export for excel files

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Computer, IT, Mechanical, Electronics)
-class ViewAdmin(admin.ModelAdmin):
+class ViewAdmin(ImportExportModelAdmin):
 	pass
